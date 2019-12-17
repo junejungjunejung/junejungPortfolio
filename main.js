08e4ee7b7faa40fe6f49bcc36d8c38792551193c
June Jung <costalgia@gmail.com>
2019-12-16 21:48:32 -0800

    make carousel automatic

--- main.js
+++ main.js
@@ -42,76 +42,66 @@ for (i = 0; i < acc.length; i++) {
 
 
 // carousel 1
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("carousel-item1");
   var dots = document.getElementsByClassName("dot1");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
 
 // carousel 2
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
+var slideIndex2 = 0;
+showSlides2();
 
-function currentSlide2(n) {
-  showSlides2(slideIndex2 = n);
-}
-
-function showSlides2(n) {
+function showSlides2() {
   var i;
   var slides = document.getElementsByClassName("carousel-item2");
   var dots = document.getElementsByClassName("dot2");
-  if (n > slides.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {slideIndex2 = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex2-1].style.display = "block";
+  slides[slideIndex2-1].style.display = "block";  
   dots[slideIndex2-1].className += " active";
+  setTimeout(showSlides2, 2000); // Change image every 2 seconds
 }
 
 
 // carousel 3
-var slideIndex3 = 1;
+var slideIndex3 = 0;
 showSlides3(slideIndex3);
 
-function currentSlide3(n) {
-  showSlides3(slideIndex3 = n);
-}
-
-function showSlides3(n) {
+function showSlides3() {
   var i;
   var slides = document.getElementsByClassName("carousel-item3");
   var dots = document.getElementsByClassName("dot3");
-  if (n > slides.length) {slideIndex3 = 1}
-  if (n < 1) {slideIndex3 = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
+  slideIndex3++;
+  if (slideIndex3 > slides.length) {slideIndex3 = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex3-1].style.display = "block";
+  slides[slideIndex3-1].style.display = "block";  
   dots[slideIndex3-1].className += " active";
+  setTimeout(showSlides3, 2000); // Change image every 2 seconds
 }
 
